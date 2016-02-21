fa52bf9cdcdc2d833ed843953f17b18b6a020122
pi3dot14 <serg1385@bk.ru>
2016-02-21 11:48:04 +0300

    download all files local project git,local

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Изучаем основы GIT'a ;-)</title>
+	<link href="styles/style.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div id="header">
+      <h1>
+        <a href="#">
+          GIT - Кто работает без гит тот совсем неочень!
+          </a>
+      </h1>
+    </div>
+	<div id="main">
+  <div id="navigation">
+    СайдБар
+  </div>
+  <div id="page">
+    <h2>Основная страница</h2>
+    <p>Привет, мир!</p>
+    <ul>
+      <li><a href="index.php">Главная</a></li>
+      <li><a href="#">Кудато</a></li>
+      <li><a href="#">Туда</a></li>
+    </ul>
+  </div>
+</div>
+<div id="footer">Copyright <?php echo date('Y'); ?>, GIT</div>
+</body>
+</html>
